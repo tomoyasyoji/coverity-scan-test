b291493415f1a6730696884bc3cc943070681646
tomoyasyoji <tomoya.syoji@gmail.com>
2023-01-25 11:51:53 +0900

    Add global/lolcal and static modifier

--- uninitialized-variable.cpp
+++ uninitialized-variable.cpp
@@ -1,11 +1,18 @@
 #include<cstdio>
 #include<cstdlib>
 
+int g1;
+static int g2;
+
 int main()
 {
-	int v;
+	int l1;
+	static int l2;
 
-	printf("v = %d\n", v);
+	printf("g1 = %d\n", g1);
+	printf("g2 = %d\n", g2);
+	printf("l1 = %d\n", l1);
+	printf("l2 = %d\n", l2);
 
 	return EXIT_SUCCESS;
 }
