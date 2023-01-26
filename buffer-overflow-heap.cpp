223d64c34bc4f74afbfe57c85de7736037b2f504
tomoyasyoji <tomoya.syoji@gmail.com>
2023-01-26 14:19:34 +0900

    Add global/lolcal

--- buffer-overflow-heap.cpp
+++ buffer-overflow-heap.cpp
@@ -3,23 +3,36 @@
 
 #define ARRAY_SIZE	5
 
+int *a;
+
 int main()
 {
-	int *a = (int *)malloc(sizeof(int) * ARRAY_SIZE);
+	int *b;
+
+	a = (int *)malloc(sizeof(int) * ARRAY_SIZE);
 	if (a == nullptr) {
+		return EXIT_FAILURE;
+	}
+
+	b = (int *)malloc(sizeof(int) * ARRAY_SIZE);
+	if (b == nullptr) {
 		return EXIT_FAILURE;
 	}
 
 	for (int i = 0; i < ARRAY_SIZE; i++) {
 		a[i] = i + 1;
+		b[i] = i + 1;
 	}
 
 	printf("a = %p\n", a);
+	printf("b = %p\n", b);
 	for (int i = 0; i < ARRAY_SIZE + 1; i++) {
 		printf("a[%d] = %d\n", i, a[i]);
+		printf("b[%d] = %d\n", i, b[i]);
 	}
 
 	free(a);
+	free(b);
 
 	return EXIT_SUCCESS;
 }
--- buffer-overflow-stack.cpp
+++ buffer-overflow-stack.cpp
@@ -3,14 +3,35 @@
 
 #define ARRAY_SIZE	5
 
+int a[ARRAY_SIZE];
+int b[ARRAY_SIZE] = {1, 2, 3, 4, 5};
+
 int main()
 {
-	int a[ARRAY_SIZE] = {1, 2, 3, 4, 5};
+	int c[ARRAY_SIZE];
+	int d[ARRAY_SIZE] = {6, 7, 8, 9, 10};
 
 	printf("a = %p\n", a);
 	for (int i = 0; i < ARRAY_SIZE + 1; i++) {
 		printf("a[%d] = %d\n", i, a[i]);
 	}
+	printf("b = %p\n", b);
+	for (int i = 0; i < ARRAY_SIZE + 1; i++) {
+		printf("b[%d] = %d\n", i, b[i]);
+	}
+	printf("c = %p\n", c);
+	for (int i = 0; i < ARRAY_SIZE + 1; i++) {
+		printf("c[%d] = %d\n", i, c[i]);
+	}
+	printf("d = %p\n", d);
+	for (int i = 0; i < ARRAY_SIZE + 1; i++) {
+		printf("d[%d] = %d\n", i, d[i]);
+	}
+
+	a[0xff] = 0xdeadbeef;
+	b[0xff] = 0xdeadbeef;
+	c[0xff] = 0xdeadbeef;
+	d[0xff] = 0xdeadbeef;
 
 	return EXIT_SUCCESS;
 }
